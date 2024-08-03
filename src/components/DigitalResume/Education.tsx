5f4f2c36b1d5c1ba984ccba4afc6602922fb0cc1
develop-with-lalit <develop.with.lalit@gmail.com>
2024-08-03 13:44:22 -0400

    master hidden

--- src/components/DigitalResume/Education.tsx
+++ src/components/DigitalResume/Education.tsx
@@ -8,15 +8,15 @@ const Education = () => {
         Educational Background
       </h2>
       <ol className="relative border-s border-gray-200 dark:border-gray-700 p-4">
-        <li className="mb-10 ms-4">
+        {/* <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            June 2021 - July 2022
+            June 2021 - July 2023
           </time>
           <h3 className="text-md font-semibold text-gray-900 dark:text-white">
             Master of Science ( Computer Science ) From Kurukshetra University
           </h3>
-        </li>
+        </li> */}
         <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
